94146d8eb62657ddb1c381af6b1dda76abea1bb3
WRLAKSHAN <wrlakshan@gmail.com>
2022-08-09 16:09:35 +0530

    This commit for add some testing 4

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
     <div className="App">
       <ThemeProvider theme={theme}>
         <Routes>
-          <Route path='/' element={<Home />}/>
+          <Route path='/'  element={<Home />}/>
         </Routes>
 
         {/*<Route path="/job">*/}
