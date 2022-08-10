6916141e999eca1b5da2b307575b41f264ec050a
RangikaShalani <rangika2520@gmail.com>
2022-08-10 21:37:47 +0530

    This commit for creat new service card data added

--- src/Components/HomeCard/HomeServiceCard.tsx
+++ src/Components/HomeCard/HomeServiceCard.tsx
@@ -4,6 +4,7 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import {inspect} from "util";
 import {makeStyles} from "@material-ui/core/styles";
+import Grid from '@mui/material/Grid';
 
 //
 // const CardContainer = shal`
@@ -16,26 +17,134 @@ import {makeStyles} from "@material-ui/core/styles";
 //
 // `;
 
+const useStyles = makeStyles({
+    root: {
+        width:'50px',
+        height:'50px',
+        borderRadius: '10px',
+        border: '1px solid #E5E5E5',
+        boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+        marginLeft:'100px'
+    },
+    CardBody: {
+        width: '250px',
+        // borderRadius: '10px',
+        padding:'10px'
+        // backgroundColor:'pink',
+        // border: '1px solid #E5E5E5'
+
+    },
+    GridCard: {
+        // direction: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: 'flex-start'
+        // backgroundColor: 'blue'
+    }
+});
+
 export default function HomeServiceCard() {
+    const classes = useStyles();
     return (
-        <div>
-            <div style={{width:'50px', height:'50px' , backgroundColor:'red', marginBottom:'-10px' , zIndex:2} }>
+        // <div>
+        //     {[
+        //         { title : 'Find jobs', name: 'FlashWork is your source for jobs and career opportunities.Search for jobs, read career advice from Monsters job experts,and find hiring and recruiting advice.' },
+        //         { title: 'Find SSS', name: 'jjjjjjjjjjj' },
+        //         { title: 'Find AAA', name: 'kkkkkkkkkkkkkkkkkkkkk' },
+        //
+        //     ].map((item, index) => (
+        //         <Grid className={classes.GridCard} >
+        //             <Grid item xs={3} key={index} >
+        //                     {/*<div style={{width:'50px', height:'50px' , backgroundColor:'red', marginBottom:'-10px' , order:'2',marginLeft:'100px'}}>*/}
+        //                     {/*</div>*/}
+        //
+        //                     <div className={classes.CardBody} >
+        //                         <div className={classes.root}>
+        //                         </div>
+        //                         <Card color={'blue'}>
+        //                             <CardContent>
+        //                                 <Typography variant="h5" component="div" color={'secondary'} style={{paddingBottom:'10px'}}>
+        //                                     {item.title}
+        //                                 </Typography>
+        //                                 <Typography variant="body2">{item.name}</Typography>
+        //                             </CardContent>
+        //                         </Card>
+        //                     </div>
+        //             </Grid>
+        //         </Grid>
+        //     ))}
+        // </div>
 
-            </div>
-            <div>
-            <Card style={{ width: '250px', borderRadius: '10px',backgroundColor: '#ECD2F2', padding:'10px' , zIndex:1}} variant="outlined">
-                <CardContent >
-                    <Typography variant="h5" component="div" style={{paddingBottom:'10px'}}>
-                        Find jobs
-                    </Typography>
 
-                    <Typography variant="body2">
-                        FlashWork is your source for jobs and career opportunities. Search for jobs, read career advice from Monster's job experts, and find hiring and recruiting advice.
-                    </Typography>
-                </CardContent>
+        <Grid className={classes.GridCard} container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 2 }} >
+            <Grid item xs={3}  >
+                <div>
+                    {/*<div style={{width:'50px', height:'50px' , backgroundColor:'red', marginBottom:'-10px' , order:'2',marginLeft:'100px'}}>*/}
+                    {/*</div>*/}
+                    <div className={classes.root}>
+                    </div>
+                    <div>
+                        <Card className={classes.CardBody} variant="outlined">
+                            <CardContent>
+                                <Typography variant="h5" component="div" color={'secondary'} style={{paddingBottom:'10px'}}>
+                                    Find jobs
+                                </Typography>
 
-            </Card>
-            </div>
-        </div>
+                                <Typography variant="body2">
+                                    FlashWork is your source for jobs and career opportunities.
+                                    Search for jobs, read career advice from Monster's job experts,
+                                    and find hiring and recruiting advice.
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </div>
+            </Grid>
+            <Grid item xs={3}>
+                <div>
+                    {/*<div style={{width:'50px', height:'50px' , backgroundColor:'red', marginBottom:'-10px' , order:'2',marginLeft:'100px'}}>*/}
+                    {/*</div>*/}
+                    <div className={classes.root}>
+                    </div>
+                    <div>
+                        <Card className={classes.CardBody} variant="outlined">
+                            <CardContent>
+                                <Typography variant="h5" component="div" color={'secondary'} style={{paddingBottom:'10px'}}>
+                                    Find jobs
+                                </Typography>
+
+                                <Typography variant="body2">
+                                    FlashWork is your source for jobs and career opportunities.
+                                    Search for jobs, read career advice from Monster's job experts,
+                                    and find hiring and recruiting advice.
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </div>
+            </Grid>
+            <Grid item xs={3}>
+                <div>
+                    {/*<div style={{width:'50px', height:'50px' , backgroundColor:'red', marginBottom:'-10px' , order:'2',marginLeft:'100px'}}>*/}
+                    {/*</div>*/}
+                    <div className={classes.root}>
+                    </div>
+                    <div>
+                        <Card className={classes.CardBody} variant="outlined">
+                            <CardContent>
+                                <Typography variant="h5" component="div" color={'secondary'} style={{paddingBottom:'10px'}}>
+                                    Find jobs
+                                </Typography>
+
+                                <Typography variant="body2">
+                                    FlashWork is your source for jobs and career opportunities.
+                                    Search for jobs, read career advice from Monster's job experts,
+                                    and find hiring and recruiting advice.
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </div>
+            </Grid>
+        </Grid>
     );
 }
--- src/Pages/LandingPages/About.tsx
+++ src/Pages/LandingPages/About.tsx
@@ -12,10 +12,7 @@ export default class About extends Component {
   render() {
     return (
       <div>
-        <Typography variant="h1" component="h2" align="center" color="neutral">
-          <h1>Heading</h1>
-        </Typography>;
-          <Job  name="James"/>
+
       </div>
     );
   }
