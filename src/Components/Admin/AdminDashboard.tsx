ad86a6b0c2a683850531e2896bc1dd8b1a32ef68
udesh98 <um4me.98@gmail.com>
2022-08-22 10:24:28 +0530

    Admin chart updated

--- src/Components/Admin/AdminDashboard.tsx
+++ src/Components/Admin/AdminDashboard.tsx
@@ -4,14 +4,10 @@ import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import { height } from '@mui/system';
+import Typography from '@mui/material/Typography';
 
-// for chart
-import {
-  ArgumentAxis,
-  ValueAxis,
-  Chart,
-  LineSeries,
-} from '@devexpress/dx-react-chart-material-ui';
+import AssistantIcon from '@mui/icons-material/Assistant';
+import { Chart } from "react-google-charts";
 
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -26,39 +22,60 @@ const Item = styled(Paper)(({ theme }) => ({
   borderRadius: '0px 0px 30px 30px',
 }));
 
-export default function dminDashboard() {
+
+// for chart
+const data = [
+    ["User Type", "User Count", { role: "style" }],
+    ["Job Provider", 9, "#f44336"],
+    ["Job Seeker", 7, "#03a9f4"],
+    ["Manpower Agency", 15, "#ff9800"]
+];
+
+export default function AdminDashboard() {
   return (
     <Box sx={{ width: 'auto', padding: '30px'}}>
       <div>
-        <div style={{float: 'left', marginBottom: '-35px', color: '#7A3293', fontWeight: 'bold'}}>
-            <h2>Users</h2>  
+        <div style={{float: 'left', marginBottom: '-20px', fontWeight: 'bold'}}>
+            <Typography variant="h5" component="h5" fontWeight='700' color='secondary' textAlign='left'>
+                Users
+            </Typography>
         </div>
         <Grid container rowSpacing={4} columnSpacing={{ xs: 1, sm: 5, md: 10 }}>
             <Grid item xs={4}>
-            <Item>Job Provider Count <br /> <div><h1>9</h1></div></Item>
+            <Item><AssistantIcon sx={{position: 'relative', top: '12px'}} fontSize='large' color='secondary' />&nbsp;&nbsp;&nbsp;Job Provider Count <br /> <Typography variant="h1" fontWeight='bold' padding='20px 0px 0px 0px' >9</Typography></Item>
             </Grid>
             <Grid item xs={4}>
-            <Item>Job Seeker Count <br /> <div><h1>7</h1></div></Item>
+            <Item><AssistantIcon sx={{position: 'relative', top: '12px'}} fontSize='large' color='secondary' />&nbsp;&nbsp;&nbsp;Job Seeker Count <br /> <Typography variant="h1" fontWeight='bold' padding='20px 0px 0px 0px' >7</Typography></Item>
             </Grid>
             <Grid item xs={4}>
-            <Item>Manpower Agency Count <br /> <div><h1>15</h1></div></Item>
+            <Item><AssistantIcon sx={{position: 'relative', top: '12px'}} fontSize='large' color='secondary' />&nbsp;&nbsp;&nbsp;Manpower Agency Count <br /> <Typography variant="h1" fontWeight='bold' padding='20px 0px 0px 0px' >15</Typography></Item>
             </Grid>
         </Grid>
-      </div><br />
+      </div><br /><br />
 
       <div>
-        <div style={{float: 'left', marginBottom: '-35px', color: '#7A3293', fontWeight: 'bold'}}>
-            <h2>Payments</h2>  
+        <div style={{float: 'left', marginBottom: '-20px', fontWeight: 'bold'}}>
+            <Typography variant="h5" component="h5" fontWeight='700' color='secondary' textAlign='left'>
+                Payments
+            </Typography> 
         </div>
         <Grid container rowSpacing={4} columnSpacing={{ xs: 1, sm: 5, md: 10 }}>
             <Grid item xs={4}>
-            <Item>Job Provider Count <br /> <div><h1>6</h1></div></Item>
+            <Item><AssistantIcon sx={{position: 'relative', top: '12px'}} fontSize='large' color='secondary' />&nbsp;&nbsp;&nbsp;Job Provider Count <br /> <Typography variant="h1" fontWeight='bold' padding='20px 0px 0px 0px' >6</Typography></Item>
             </Grid>
             <Grid item xs={4}>
-            <Item>Job Seeker Count <br /> <div><h1>4</h1></div></Item>
+            <Item><AssistantIcon sx={{position: 'relative', top: '12px'}} fontSize='large' color='secondary' />&nbsp;&nbsp;&nbsp;Job Seeker Count <br /> <Typography variant="h1" fontWeight='bold' padding='20px 0px 0px 0px' >4</Typography></Item>
             </Grid>
             <Grid item xs={4}>
-            <Item>Manpower Agency Count <br /> <div><h1>11</h1></div></Item>
+            <Item><AssistantIcon sx={{position: 'relative', top: '12px'}} fontSize='large' color='secondary' />&nbsp;&nbsp;&nbsp;Manpower Agency Count <br /> <Typography variant="h1" fontWeight='bold' padding='20px 0px 0px 0px' >11</Typography></Item>
+            </Grid>
+        </Grid>
+      </div><br /><br />
+
+      <div>
+        <Grid container rowSpacing={4} columnSpacing={{ xs: 1, sm: 5, md: 10 }}>
+            <Grid item xs={12}>
+                <Chart chartType="ColumnChart" width="100%" height="500px" data={data} />
             </Grid>
         </Grid>
       </div>
