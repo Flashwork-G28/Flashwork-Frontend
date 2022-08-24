cb0617a63406458fa5b95eeff285f67b8f72007f
WRLAKSHAN <wrlakshan@gmail.com>
2022-08-24 11:00:31 +0530

    This Commmit for mearge chuti 1.0

--- src/Components/JobProvider/JobProfile.tsx
+++ src/Components/JobProvider/JobProfile.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import JobProviderProfileContent from "./JobProviderComponents/JobProviderProfileContent";
 import JobProviderProMember from "./JobProviderComponents/JobProviderProMember";
+import JobProviderProfileDetails from "./JobProviderComponents/JobProviderProfileDetails";
 
 const JobProfile = () => {
     return (
         <div>
             {/*<JobProviderProfileContent />*/}
 
-            <JobProviderProMember />
+            <JobProviderProfileDetails />
         </div>
     );
 };
