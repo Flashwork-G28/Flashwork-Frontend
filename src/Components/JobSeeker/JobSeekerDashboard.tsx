87bf7991a988d6ebb1ca352a49e446382b21f6c4
Lakshitha Dhananjaya <lakshithanew1998@gmail.com>
2022-10-26 10:46:23 +0530

    this is JobSeekerProfileNew2

--- src/Components/JobSeeker/JobSeekerDashboard.tsx
+++ src/Components/JobSeeker/JobSeekerDashboard.tsx
@@ -34,6 +34,7 @@ const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
     ...theme.typography.body2,
     padding: theme.spacing(1),
+
     textAlign: 'center',
     color: '#1C1C1C',
     //color: theme.palette.text.secondary,
